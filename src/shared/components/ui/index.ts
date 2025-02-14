e766012a8f0dea8af69f6a0063628cc4a8d53cbe
annaAzh <forarre@yandex.ru>
2025-02-14 12:52:58 +0300

    feat: add ui for search input, base input and select

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -1,2 +1,4 @@
 export { Button } from './button';
 export { Container } from './container';
+export { Input } from './input';
+export { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from './select';
