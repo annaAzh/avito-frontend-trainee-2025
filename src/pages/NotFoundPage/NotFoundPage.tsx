b8781cccb4cb3f01e973f0be1ebf353bdff0fa5f
annaAzh <forarre@yandex.ru>
2025-02-14 22:43:37 +0300

    feat: create ui for not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,26 @@
+import { Button, Container } from '@/shared/components/ui';
+import { Paths } from '@/shared/types';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 export const NotFoundPage: FC = () => {
-  return <div>Not found page</div>;
+  return (
+    <div className="min-h-screen flex flex-col relative">
+      <main className="m-auto">
+        <Container>
+          <div className="py-6 flex flex-col items-center">
+            <div className="text-primary uppercase mb-6 md:mb-10 text-9xl font-medium font-rubik ">404</div>
+            <p className="font-rubik text-3xl md:text-4xl md:mb-4">Упс!</p>
+            <p className="font-rubik text-3xl md:text-4xl mb-4">Страница не найдена</p>
+            <p className="font-inter text-base text-gray-400 md:text-xl mb-8 text-center">
+              Эта страница не существует или была удалена
+            </p>
+            <Button className="w-fit m-auto">
+              <Link to={Paths.HOME}>Вернуться на главную</Link>
+            </Button>
+          </div>
+        </Container>
+      </main>
+    </div>
+  );
 };
